1b4eb57e0dfd15e720971c2ced1ea37f8c024d89
Oran Simhony <oran.simhony@gmail.com>
2020-11-27 17:07:55 +0200

    Added docstrings to io.h

--- include/io.h
+++ include/io.h
@@ -1,9 +1,17 @@
+/** @file io.h */
 #ifndef __IO_H__
 #define __IO_H__
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Get the contents of a file.
+ *
+ * @param[in] file_path the path to the file.
+ *
+ * @return the contents of the file.
+ */
 char *IO_get_file_contents(const char *file_path);
 
 #endif // __IO_H__
