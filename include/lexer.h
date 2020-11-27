578763104a168102b83314263f6079d8b00b390a
Oran Simhony <oran.simhony@gmail.com>
2020-11-27 17:13:00 +0200

    Added docstrings to lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -1,3 +1,4 @@
+/** @file lexer.h */
 #ifndef __LEXER_H__
 #define __LEXER_H__
 
@@ -5,6 +6,19 @@
 #include "logger.h"
 #include "vector.h"
 
+/**
+ * @brief tokenize Luka source code.
+ *
+ * @param[out] tokens the vector of tokens.
+ * @param[in] source the source code.
+ * @param[in] logger a logger that can be used to log messages.
+ *
+ * @return
+ * * LUKA_SUCCESS on success,
+ * * LUKA_CANT_ALLOC_MEMORY on memory allocation failure,
+ * * LUKA_VECTOR_FAILURE when failed to add a token to the vector
+ * * LUKA_LEXER_FAILED if failed to lex identifier or string.
+ */
 t_return_code LEXER_tokenize_source(t_vector *tokens, const char *source, t_logger *logger);
 
 #endif // __LEXER_H__
